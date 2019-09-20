d1661685c7752345f8b2df9a1c0cf448fa53cbe6
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2019-09-19 21:40:04 -0400

    Adds a completed version of Blacksmith

--- blacksmith.js
+++ blacksmith.js
@@ -34,7 +34,12 @@ const settings = {
  * The game object stores the current game status
  */
 const game = {
-
+  gold: 10,
+  ore: 0,
+  wood: 0,
+  swords: 0,
+  axes: 0,
+  fire: false
 }
 
 /**
@@ -46,7 +51,18 @@ const game = {
  * To stop a fire:
  *    The fire must be going
  */
-
+function fire () {
+  if (game.fire) {
+    game.fire = false
+    return 'You have put out the fire.'
+  } else if (game.wood > settings.fireWood) {
+    game.wood -= settings.fireWood
+    game.fire = true
+    return 'You have started the fire.'
+  } else {
+    return 'You do not have enough wood.'
+  }
+}
 /**
  * buy
  * To buy wood or ore
@@ -56,6 +72,23 @@ const game = {
  *    The player must have enough gold
  *    The player will on receive 1 item
  */
+function buy (item) {
+  if (game.fire === false) {
+    if (item === 'ore' && game.gold >= settings.oreCost) {
+      game.ore++
+      game.gold -= settings.oreCost
+      return 'You bought 1 piece of ore.'
+    } else if (item === 'wood' && game.gold >= settings.woodCost) {
+      game.wood++
+      game.gold -= settings.woodCost
+      return 'You bought 1 piece of wood.'
+    } else {
+      return `You cannot buy a(n) ${item}.`
+    }
+  } else {
+    return 'You must put out the fire.'
+  }
+}
 
 /**
  * make
@@ -66,6 +99,25 @@ const game = {
  *    The player must have enough wood and ore
  *    The player will make 1 item
  */
+function make (item) {
+  if (game.fire) {
+    if (item === 'sword' && game.ore >= settings.swordOre && game.wood >= settings.swordWood) {
+      game.wood -= settings.swordWood
+      game.ore -= settings.swordOre
+      game.swords++
+      return 'You have made 1 sword.'
+    } else if (item === 'axe' && game.ore >= settings.axeOre && game.wood >= settings.axeWood) {
+      game.wood -= settings.axeWood
+      game.ore -= settings.axeOre
+      game.axes++
+      return 'You have made 1 axe.'
+    } else {
+      return `You cannot make a(n) ${item}.`
+    }
+  } else {
+    return 'You must start the fire.'
+  }
+}
 
 /**
  * sell
@@ -78,11 +130,45 @@ const game = {
  *   The player will receive a random value based on the
  *   price range
  */
-
+function sell (item) {
+  if (!game.fire) {
+    if (item === 'sword' && game.swords > 0) {
+      game.swords--
+      const price = randomInt(settings.swordPriceMin, settings.swordPriceMax)
+      game.gold += price
+      return `You sold 1 ${item} for ${price} pieces of gold.`
+    } else if (item === 'axe' && game.axes > 0) {
+      game.axes--
+      const price = randomInt(settings.axePriceMin, settings.axePriceMax)
+      game.gold += price
+      return `You sold 1 ${item} for ${price} pieces of gold.`
+    } else {
+      return `You do not have a(n) ${item}.`
+    }
+  } else {
+    return 'You must put out the fire.'
+  }
+}
 /**
  * inventory
  * Shows the players current inventory
  */
+function inventory () {
+  const response = []
+  response.push('INVENTORY:\n')
+  for (const item in game) {
+    if (item === 'fire') {
+      if (game[item]) {
+        response.push('The fire is burning.')
+      } else {
+        response.push('The fire is not burning.')
+      }
+    } else {
+      response.push(`${item}: ${game[item]}\n`)
+    }
+  }
+  return response.join('')
+}
 
 /**
  * Help Command
@@ -90,17 +176,17 @@ const game = {
  */
 function help () {
   return `INSTRUCTIONS:
-Blacksmith is a simple text base game. 
-
-As a blacksmith you convert ore and wood into swords and axes. You buy your resources using gold and sell your weapons for gold.
-
-COMMANDS:
-- buy(item)
-- make(item)
-- sell(item)
-- fire()
-- inventory()
-- help()`
+  Blacksmith is a simple text base game. 
+  
+  As a blacksmith you convert ore and wood into swords and axes. You buy your resources using gold and sell your weapons for gold.
+  
+  COMMANDS:
+  - buy(item)
+  - make(item)
+  - sell(item)
+  - fire()
+  - inventory()
+  - help()`
 }
 
 // Log the help() function
